dc4d2411941d7356ab9ff1c020818c1a7cbd117b
Ömür Efe Güçlü <omurefeguclu2004@gmail.com>
2021-07-23 00:08:11 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+
+  }
+});
